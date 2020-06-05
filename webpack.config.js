9c3a433bf8a22266277ec15b547bc922dac954ad
Hanna Song <gwiyom93@gmail.com>
2020-06-05 23:55:18 +1200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/app.js",
+  mode: "development",
+  output: {
+    filename: "bundle.js",
+    path: path.join(__dirname, "public")
+  },
+  module: {
+    rules: [
+      {
+        loader: "babel-loader",
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public")
+  }
+};
